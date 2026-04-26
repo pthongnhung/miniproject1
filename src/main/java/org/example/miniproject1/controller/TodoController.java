4ebffd79ced2b15b66d2be293adfe92d687a10fa
pthongnhung <pthongnhung13122006@gmail.com>
2026-04-26 20:48:13 +0700

    add

--- src/main/java/org/example/miniproject1/controller/TodoController.java
+++ src/main/java/org/example/miniproject1/controller/TodoController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -43,4 +46,60 @@ public class TodoController {
         todoRepository.save(todo);
         return "redirect:/";
     }
+
+    // Hiển thị form sửa
+    @GetMapping("/edit/{id}")
+    public String viewEdit(
+            @PathVariable Long id,
+            Model model
+    ) {
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+
+        if(optionalTodo.isPresent()){
+            model.addAttribute("todo", optionalTodo.get());
+            return "todo-edit";
+        }
+
+        return "redirect:/";
+    }
+
+    // Lưu dữ liệu sửa
+    @PostMapping("/update")
+    public String update(
+            @Valid @ModelAttribute("todo") Todo todo,
+            BindingResult result
+    ) {
+        if(result.hasErrors()){
+            return "todo-edit";
+        }
+
+        todoRepository.save(todo);
+        return "redirect:/";
+    }
+
+//    Xóa
+@GetMapping("/delete/{id}")
+public String delete(
+        @PathVariable Long id,
+        RedirectAttributes redirectAttributes
+) {
+
+    if (!todoRepository.existsById(id)) {
+        redirectAttributes.addFlashAttribute(
+                "message",
+                "Task không tồn tại!"
+        );
+        return "redirect:/";
+    }
+
+    todoRepository.deleteById(id);
+
+    redirectAttributes.addFlashAttribute(
+            "message",
+            "Xóa thành công!"
+    );
+
+    return "redirect:/";
+}
+
 }
